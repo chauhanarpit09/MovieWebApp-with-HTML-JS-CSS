f6e91e2f203fafd04b69a4d8fd92bc5cb017c0ff
Arpit Chauhan <arpit09chauhan@gmail.com>
2021-06-02 16:55:49 +0530

    262021

--- js/index.js
+++ js/index.js
@@ -78,7 +78,7 @@ function sliderInit(){
                             {
                             breakpoint: 1024,
                             settings: {
-                                slidesToShow: 3,
+                                slidesToShow: 4,
                                 slidesToScroll: 3,
                                 infinite: true,
                                 dots: true
@@ -87,7 +87,7 @@ function sliderInit(){
                             {
                             breakpoint: 600,
                             settings: {
-                                slidesToShow: 2,
+                                slidesToShow: 3,
                                 slidesToScroll: 2
                             }
                             },
